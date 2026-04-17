8025a5f6a52d519b935e92a2d6df1f19bea3f487
Rituparna Rath <rituparnar@mindfiresolutions.com>
2026-04-17 11:42:43 +0530

    feat(storybook): add button stories and fix accessibility issues

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,16 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
- 
-export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    files: ['**/*.js', '**/*.ts'],
-  },
- 
-  {
-    ignores: [
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/.husky/**'
-    ]
-  }
-]
+
+export default [js.configs.recommended, ...tseslint.configs.recommended, {
+  files: ['**/*.js', '**/*.ts'],
+}, {
+  ignores: [
+    '**/node_modules/**',
+    '**/dist/**',
+    '**/.husky/**'
+  ]
+}, ...storybook.configs["flat/recommended"]];
  
---
+++ storybook/.storybook/preview.ts
@@ -0,0 +1,11 @@
+import '../../packages/core/ignix-lite.min.css'
+
+export const parameters = {
+  backgrounds: {
+    default: 'light',
+    values: [
+      { name: 'light', value: '#ffffff' },
+      { name: 'dark', value: '#111827' }
+    ]
+  }
+}
