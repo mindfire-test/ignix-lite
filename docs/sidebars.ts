5043132d856f3436463f931c2dcdfc1de7f3a6ec
Rituparna Rath <rituparnar@mindfiresolutions.com>
2026-05-07 17:23:50 +0530

    feat(setup): initial setup of docusaurus

---
+++ docs/sidebars.ts
@@ -0,0 +1,43 @@
+const sidebars = {
+  tutorialSidebar: [
+
+    {
+      type: "category",
+      label: "Getting Started",
+      items: [
+        "introduction" 
+      ]
+    },
+
+    {
+      type: "category",
+      label: "Components",
+      items: [
+        "components/button",
+        "components/input",
+        "components/checkbox",
+        "components/radio",
+        "components/badge",
+        "components/dialog",
+        "components/form",
+        "components/select",
+        "components/alert",
+        "components/avatar",
+        "components/meter",
+        "components/progress",
+        "components/codeblock",
+        "components/tooltip"
+      ]
+    },
+    {
+      type: "category",
+      label: "Contribution Guide",
+      items: [
+        "contribution-guide/how-to-contribute",
+        "contribution-guide/code-of-conduct"
+      ]
+    }
+  ]
+}
+
+export default sidebars
